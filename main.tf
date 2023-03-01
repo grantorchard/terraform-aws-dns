3c4ed331432fb7e0458bb43faebd809ec940f11d
Grant Orchard <go@hashicorp.com>
2023-03-01 13:10:01 +1100

    update provider and move to new zone

--- main.tf
+++ main.tf
@@ -11,26 +11,6 @@ provider "aws" {
  }
 }
 
-data "aws_route53_zone" "main" {
-  name = "hashidemos.io"
+resource "aws_route53_zone" "this" {
+  name = "go.aws.hashidemo.io"
 }
-
-resource "aws_route53_zone" "aws_sub_zone" {
-  for_each = toset(var.sub_zone)
-  name    = each.value
-  comment = "Delegated Sub Zone for AWS for go.hashidemos.io"
-}
-
-resource "aws_route53_record" "aws_sub_zone_ns" {
-  for_each = toset(var.sub_zone)
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = each.value
-  type    = "NS"
-  ttl     = "30"
-
-  records = [
-    for zone in aws_route53_zone.aws_sub_zone[each.value].name_servers :
-    zone
-  ]
-}
-
